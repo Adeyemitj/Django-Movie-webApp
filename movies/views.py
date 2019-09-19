0758ecc9d932f07e0d9b93b16ff958221aa30df5
Taiwo <adetwins2005@yahoo.com>
2019-09-19 17:14:40 +0000

    Created Django Movie Web App

--- movies/views.py
+++ movies/views.py
@@ -17,4 +17,50 @@ def home_page(request):
 
 
 def create(request):
+    if request.method == 'POST':
+        data = {
+            'Name':request.POST.get('name'),
+            'Pictures':[{'url':request.POST.get('url') or 'https://wingslax.com/wp-content/uploads/2017/12/no-image-available.png'}],
+            'Rating':int(request.POST.get('rating')),
+            'Notes': request.POST.get('notes'),
+        }
+        try:
+            response = AT.insert(data)
+            messages.success(request, 'New movie: {}, added successfully'.format(response['fields'].get('Name'))) #Notify on create NB: the 'Name' is stored into {}
+        except Exception as e:
+            messages.warning(request, 'Got an error when trying to update a movie: {}'.format(e))
+
     return redirect('/')
+
+
+def edit(request, movie_id):
+    if request.method == 'POST':
+        data = {
+            'Name': request.POST.get('name'),
+            'Pictures': [{'url': request.POST.get('url') or 'https://wingslax.com/wp-content/uploads/2017/12/no-image-available.png'}],
+            'Rating': int(request.POST.get('rating')),
+            'Notes': request.POST.get('notes'),
+        }
+        try:
+            response = AT.update(movie_id, data)
+            messages.success(request, "movie: {} updated successfuly".format(response['fields'].get('Name')))
+        except Exception as e:
+            messages.warning(request, "Got an error when trying to update a movie: {}".format(e))
+
+    return redirect('/')
+
+def delete(request, movie_id):
+    try:
+        movie_name = AT.get(movie_id)['fields'].get('Name')
+        AT.delete(movie_id) #AT.delete(movie_id)
+        #notify on delete
+        messages.warning(request, 'Deleted movie: {}'.format(movie_name))
+    except Exception as e:
+        messages.warning(request, 'Got an error when trying to delete {}'.format(e))
+
+
+
+
+    return redirect('/')
+
+
